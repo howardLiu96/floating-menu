78b35790c269f9b2f9a1164775563bfc70bb4829
howard <hliu1461603525@gmail.com>
2020-10-06 23:27:06 -0700

    Improve sidebar experience: move floating buttion along with popping sidebar

--- script/floating_menu.js
+++ script/floating_menu.js
@@ -10,38 +10,61 @@
 // @include         *
 // ==/UserScript==
 
+function showMenu()
+{
+    if (!isMenuOut) {
+        f_container.style.display = "block";
+        isMenuOut = true;
+        showMenuButton.textContent = "Menu>";
+        showMenuButton.style.right = "260px";
+        backButton.style.right = "260px";
+    } else {
+        f_container.style.display = "none";
+        isMenuOut = false;
+        showMenuButton.textContent = "Menu<";
+        showMenuButton.style.right = "30px";
+        backButton.style.right = "30px";
+    }
+}
 
-var input=document.createElement("button");
-input.type="button";
-input.textContent="Menu";
-input.setAttribute("id", "test");
-input.onclick = showAlert;
-document.body.appendChild(input);
+function scrollUp() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backButton.style.display = "block";
+        showMenuButton.style.display = "block";
+    } else {
+        backButton.style.display = "none";
+        showMenuButton.style.display = "none";
+    }
+}
 
-var menu = document.getElementsByClassName("panel expanded PanelsUserGuide UserGuide");
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
+// showMenuButton setup
+var showMenuButton = document.createElement("button");
+showMenuButton.type = "button";
+showMenuButton.textContent = "Menu<";
+showMenuButton.setAttribute("id", "test");
+showMenuButton.onclick = showMenu;
+document.body.appendChild(showMenuButton);
+
+var tocElement = document.getElementsByClassName("panel expanded PanelsDocumentationLinks DocumentationLinks");
+var menu = tocElement[0].cloneNode(true);
+
+// setup floating container
 var f_container = document.createElement("div");
 f_container.setAttribute('id', 'f_container');
 f_container.setAttribute('class', 'f_container');
 document.body.appendChild(f_container)
 
-var isMenuOut = false;
-
-document.getElementById('f_container').appendChild(menu[0]);
+document.getElementById('f_container').appendChild(menu);
 document.getElementById('f_container').style.width = "250px";
 
-function showAlert()
-{
-    if (!isMenuOut) {
-        f_container.style.display = "block";
-        isMenuOut = true;
-    } else {
-        f_container.style.display = "none";
-        isMenuOut = false;
-    }
-    // alert("Hello World");
-}
+var isMenuOut = false;
 
+// backButton setup
 var backButton = document.createElement("button");
 backButton.type = "button";
 backButton.textContent = "Top"
@@ -50,21 +73,6 @@ backButton.setAttribute("id", "myBtn");
 document.body.appendChild(backButton);
 
 window.onscroll = function() {scrollUp()};
-
-function scrollUp() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        backButton.style.display = "block";
-        input.style.display = "block";
-    } else {
-        backButton.style.display = "none";
-        input.style.display = "none";
-    }
-}
-
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
 
 GM_addStyle (`
     .f_container {
@@ -75,7 +83,7 @@ GM_addStyle (`
         z-index: 1;
         top: 0;
         right: 0;
-        background-color: #111;
+        background-color: #FFF;
         overflow-x: hidden;
         transition: 0.5s;
         padding-top: 60px;
